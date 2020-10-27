81c6b2ebd70ee171e83645df0d795a6d2e156ea2
Mariusz Bugara <mariusz.bugara@gmail.com>
2020-10-27 16:13:05 +0100

    Updates

---
+++ precache-manifest.940af8673741f412e5085a70256f78b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a6d8bab68669fe269565b6b78237d4",
+    "url": "/winery-website/index.html"
+  },
+  {
+    "revision": "d6f1854d72a181e10eaf",
+    "url": "/winery-website/static/css/main.48afe90f.chunk.css"
+  },
+  {
+    "revision": "e316d933224255077ecd",
+    "url": "/winery-website/static/js/2.5b1cabeb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/winery-website/static/js/2.5b1cabeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f1854d72a181e10eaf",
+    "url": "/winery-website/static/js/main.db85d5cd.chunk.js"
+  },
+  {
+    "revision": "48b61fd61ed52cc440fa",
+    "url": "/winery-website/static/js/runtime-main.551c2c99.js"
+  },
+  {
+    "revision": "17a12ebe608e6d5a53ff8827b1629ca9",
+    "url": "/winery-website/static/media/fontello26e8.17a12ebe.svg"
+  },
+  {
+    "revision": "ce12acc0e2d1c1007ccda05ccef61cb6",
+    "url": "/winery-website/static/media/fontello26e8.ce12acc0.eot"
+  }
+]);
