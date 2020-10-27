b1a2cd48ccbcd620a2e9e40e59dda598983871f4
Mariusz Bugara <mariusz.bugara@gmail.com>
2020-10-27 14:02:57 +0100

    Updates

---
+++ precache-manifest.ee4eb08e74bc663da15dc3435ff34882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105b87be62a1009bac9034b95d02579d",
+    "url": "/winery-website/index.html"
+  },
+  {
+    "revision": "1d00387607879f5c1d96",
+    "url": "/winery-website/static/css/main.52d5a631.chunk.css"
+  },
+  {
+    "revision": "a1db4a1cf85fbff183cc",
+    "url": "/winery-website/static/js/2.badfa243.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/winery-website/static/js/2.badfa243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d00387607879f5c1d96",
+    "url": "/winery-website/static/js/main.84da7f3c.chunk.js"
+  },
+  {
+    "revision": "48b61fd61ed52cc440fa",
+    "url": "/winery-website/static/js/runtime-main.551c2c99.js"
+  }
+]);
