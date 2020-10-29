f49f4481ac800ea635e5cdd5c8d0ed20dfb3bdd4
Mariusz Bugara <mariusz.bugara@gmail.com>
2020-10-29 13:08:48 +0100

    Fixed and changed logo in footer

--- src/winery/components/Footer/index.tsx
+++ src/winery/components/Footer/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {PageElement} from "../PageWrapper";
+import {FileUtils} from "../shared/Utils";
+import {NavLink} from "react-router-dom";
 
 const Footer: React.FC<PageElement> = () => {
 
@@ -8,7 +10,11 @@ const Footer: React.FC<PageElement> = () => {
         <footer className="site_footer_wrap scheme_dark">
             <div className="logo_footer_wrap scheme_dark">
                 <div className="logo_footer_wrap_inner">
-                    <img src="/images/logo.png" className="logo_footer_image" alt=""/>
+                    <img src={FileUtils.getImage("logo-footer.png")}
+                         className="logo_footer_image"
+                         alt="logo-bugara"
+                         style={{maxHeight: "279px"}}
+                    />
                 </div>
             </div>
             <div className="socials_footer_wrap socials_wrap scheme_dark">
@@ -23,13 +29,13 @@ const Footer: React.FC<PageElement> = () => {
                 <div className="menu_footer_wrap_inner">
                     <nav className="menu_footer_nav_area">
                         <ul id="menu_footer" className="menu_footer_nav">
-                            <li className="menu-item menu-item-143"><a href="home3.html"><span>Start</span></a></li>
-                            <li className="menu-item menu-item-675"><a href="about-us.html"><span>Nasza filozofia</span></a>
+                            <li className="menu-item menu-item-143"><NavLink to={"/"}><span>Start</span></NavLink></li>
+                            <li className="menu-item menu-item-675"><NavLink to={"/karta_win"}><span>Wina</span></NavLink>
                             </li>
-                            <li className="menu-item menu-item-677"><a href="wine-list.html"><span>Co tworzymy</span></a>
+                            <li className="menu-item menu-item-677"><NavLink to={"/"}><span>Nasza filozofia</span></NavLink>
                             </li>
-                            <li className="menu-item menu-item-147"><a href="shop.html"><span>Sklep</span></a></li>
-                            <li className="menu-item menu-item-676"><a href="contacts.html"><span>Polityka prywatności</span></a>
+                            <li className="menu-item menu-item-147"><NavLink to={"/"}><span>Winnica</span></NavLink></li>
+                            <li className="menu-item menu-item-676"><NavLink to={"/"}><span>Kontakt</span></NavLink>
                             </li>
                         </ul>
                     </nav>
