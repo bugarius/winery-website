74022af9ee4c77d86390fbd8bad650da68bfdba6
Mariusz Bugara <mariusz.bugara@gmail.com>
2020-11-30 21:31:54 +0100

    Fixed Loader overlay

--- src/winery/components/ContactUs/EmailForm/SendingEmailContainer.tsx
+++ src/winery/components/ContactUs/EmailForm/SendingEmailContainer.tsx
@@ -1,14 +1,19 @@
-import React, {ChangeEvent, FC, useCallback, useReducer} from "react";
+import React, {ChangeEvent, FC, useCallback, useReducer, useState} from "react";
 import {useEmailService} from "./useEmailService";
 import {EmailState} from "./EmailFormPresentation";
 import {FormValidator} from "../../forms/validators/FormValidator";
 
 const reducer = (state: EmailState, action: { type: string, value?: any }) => {
-    if (action.type === "reset")
+    switch (action.type)
     {
-        return {...defaultState, submitMessage: state.submitMessage};
+        case "reset":
+            return {...defaultState, submitMessage: state.submitMessage};
+        case "resetSubmitMessage":
+            return {...state, submitMessage: defaultState.submitMessage};
+        default:
+            return {...state, [action.type]: action.value};
+
     }
-    return {...state, [action.type]: action.value};
 };
 
 const defaultState = {
@@ -27,13 +32,15 @@ interface Props
     render: (
         handleChange: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
         handleSubmit: (event: React.MouseEvent<HTMLElement>) => void,
-        state: EmailState
+        state: EmailState,
+        loading: boolean
     ) => JSX.Element;
 }
 
 export const SendingEmailContainer: FC<Props> = ({render}) => {
 
     const [state, dispatch] = useReducer(reducer, defaultState);
+    const [loading, setLoading] = useState(false);
 
     const {sendEmail} = useEmailService({
         name: state.name,
@@ -50,8 +57,10 @@ export const SendingEmailContainer: FC<Props> = ({render}) => {
         e.preventDefault();
         const isFormValid = FormValidator.notAllBlank([state.name, state.email, state.message]) && FormValidator.isEmailValid(state.email);
         dispatch({type: "triggerSend", value: true});
+        dispatch({type: "resetSubmitMessage"});
         if (isFormValid)
         {
+            setLoading(true);
             sendEmail()
                 .then(handleSendEmail)
                 .catch(handleError)
@@ -66,6 +75,7 @@ export const SendingEmailContainer: FC<Props> = ({render}) => {
         dispatch({type: "triggerSend", value: false});
         dispatch({type: "submitMessage", value: {isValid: true, message: "Wiadomość wysłana!"}});
         dispatch({type: "reset"});
+        setLoading(false);
     }
 
     const handleError = () => {
@@ -77,8 +87,9 @@ export const SendingEmailContainer: FC<Props> = ({render}) => {
                 message: "Błąd wysyłania wiadomości! Spróbuj ponownie, lub skontaktuj się na facebook'u"
             }
         });
+        setLoading(false);
     }
 
-    return render(handleChange, handleSubmit, state)
+    return render(handleChange, handleSubmit, state, loading)
 
 };
--- src/winery/components/ContactUs/EmailForm/index.tsx
+++ src/winery/components/ContactUs/EmailForm/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {EmailFormPresentation} from "./EmailFormPresentation";
 import {SendingEmailContainer} from "./SendingEmailContainer";
+import {Loader} from "../../../commons/components/Loader/loader";
 
 export const EmailForm = () => {
     return (
-        <SendingEmailContainer render={(onChange, onSubmit, state) => (
-            <EmailFormPresentation onChange={onChange} state={state} onSubmit={onSubmit}/>
+        <SendingEmailContainer render={(onChange, onSubmit, state, loading) => (
+            <Loader loading={loading}><EmailFormPresentation onChange={onChange} state={state} onSubmit={onSubmit}/></Loader>
         )}/>
     )
 };
