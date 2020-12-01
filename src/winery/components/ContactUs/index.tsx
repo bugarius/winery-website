90c607ca83e563698a32d10434704c5f98d594d3
Mariusz Bugara <mariusz.bugara@gmail.com>
2020-12-01 12:37:47 +0100

    Modified contact us content

--- src/winery/components/ContactUs/index.tsx
+++ src/winery/components/ContactUs/index.tsx
@@ -3,35 +3,29 @@ import {GoogleMapContact} from "./GoogleMapContact";
 import {RowContent} from "../RowContent";
 import {CommonWrapper} from "../wrappers/CommonWrapper";
 import {EmailForm} from "./EmailForm";
+import {useTranslation} from "react-i18next";
 
 export const ContactUs: FC<{}> = () => {
+
+    const {t} = useTranslation();
 
     return (
         <div className="page_content_wrap scheme_default">
             <GoogleMapContact/>
             <CommonWrapper>
                 <RowContent icon={"icon-winery"}
-                            title={"Sprzedaż przy winnicy"}
-                            subtitle={"Jeśli chcesz do nas przyjechać, to zapraszamy o każdej rozsądej godzinie. " +
-                            "Godziny otwarcia są tylko poglądowe. Do nas można wpaść jak do dobrego znajomego :)"}
-                            paragraphAsDiv
-                            paragraphs={[
-                                "Godziny otwarcia:",
-                                "Pon-Pt 9:00 - 18:00",
-                                "Sob 10:00 - 17:00",
-                                "Niedz 10:00 - 15:00",
-                                "~",
-                                "Jeśli wpadniesz do nas a winiarnia będzie zamknięta, to jest wielce prawdopodobne, że ktoś " +
-                                "właśnie jest na winnicy lub wypełnia inne obowiązki i w ciągu paru chwil może przyjść Ci z pomocą. " +
-                                "Wystarczy, że zadzwonisz na jeden z naszych numerów.",
-                                "~",
-                                "Jeżeli chcesz mieć 100% pewności, że od razu zastaniesz nas w punkcie sprzedaży, to zapraszamy " +
-                                "do wcześniejszego kontaktu telefonicznego, lub bezpośredniej wiadomości na naszym facebooku."
-                            ]}
+                            title={t('CONTACT_US.RETAIL.T1')}
+                            subtitle={t('CONTACT_US.RETAIL.SBT1')}
+                            paragraphs={[t('CONTACT_US.RETAIL.P1')]}
+                />
+                <RowContent icon={"icon-grape-3"}
+                            title={t('CONTACT_US.RETAIL.T2')}
+                            subtitle={t('CONTACT_US.RETAIL.SBT2')}
+                            paragraphs={[t('CONTACT_US.RETAIL.P2.1'), t('CONTACT_US.RETAIL.P2.2')]}
                 />
                 <RowContent icon={"icon-grape-leafs"}
-                            title={"Skontaktuj się"}
-                            subtitle={"Jeśli masz pytania odnoście naszych win, degustacji, zwiedzania, zakupów zapraszamy do kontaktu"}
+                            title={t('CONTACT_US.RETAIL.T3')}
+                            subtitle={t('CONTACT_US.RETAIL.SBT3')}
                 />
                 <EmailForm/>
             </CommonWrapper>
